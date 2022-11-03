ca2306b9f896c7ed7482b8c0dd69700908adbfee
mlcundayag <mlcundayag@gmail.com>
2022-11-02 20:26:50 -1000

    rnder pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import Header from "./components/Header"
-import Main from "./components/Main"
+import Header from "./components/Body";
 // import Footer from "./components/Footer"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Main>Hi</Main>
-      {/* <Footer>Hello</Footer> */}
+      <header>
+        <Header />
+      </header>
     </div>
   );
 }
---
+++ src/components/Body/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import logo from "../../assets/images/logo.png";
+
+import Nav from "../Nav";
+import About from "../About";
+import Portfolio from "../Portofolio";
+import Contact from "../Contact";
+import Resume from "../Resume";
+
+import "./style.css";
+
+function Header() {
+    const [sections] = useState([
+        { name: "About Me" },
+        { name: "Portfolio" },
+        { name: "Contact Me" },
+        { name: "My Resume" }
+    ])
+
+    const [currentSection, setCurrentSection] = useState(sections[0])
+
+    const renderPage = () => {
+        if (currentSection.name === "About Me") {
+            return <About />
+        } else if (currentSection.name === "Portfolio") {
+            return <Portfolio />
+        } else if (currentSection.name === "Contact Me") {
+            return <Contact />
+        } else if (currentSection.name === "My Resume") {
+            return <Resume />
+        } else {
+            return <About />
+        }
+    }
+    return (
+        <div>
+            <header className="custom-header px-3">
+                <div>
+                    <a>
+                        <img className="py-2" src={logo} alt="logo" height="100" />
+                        <h1 className="header">easy<span className="text-green">Coder</span></h1>
+                    </a>
+                </div>
+                <Nav sections={sections} setCurrentSection={setCurrentSection} currentSection={currentSection} />
+            </header>
+            <main>
+                <div>
+                    {renderPage()}
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default Header;
